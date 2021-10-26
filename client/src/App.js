dc8391316b8dd90e88b828c59fc194eff2bba33e
LeeGyuHwang <el@Appleui-MacBookAir.local>
2021-10-26 16:24:40 +0900

    add Header ,footer css

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,35 @@
-import React from 'react';
+import React from 'react'
 import {
   BrowserRouter as Router,
   Route,
   Redirect,
   Switch
-} from 'react-router-dom';
-import Chat from './pages/Chat';
-import CreatePost from './pages/CreatePost';
-import DetailPost from './pages/DetailPost';
-import EditPost from './pages/EditPost';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import MainPage from './pages/MainPage';
-import MyPage from './pages/MyPage';
-import EditMyPage from './pages/EditMyPage';
-import Navbar from './components/Navbar';
+} from 'react-router-dom'
+import Chat from './pages/Chat'
+import CreatePost from './pages/CreatePost'
+import DetailPost from './pages/DetailPost'
+import EditPost from './pages/EditPost'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import MainPage from './pages/MainPage'
+import MyPage from './pages/MyPage'
+import EditMyPage from './pages/EditMyPage'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 function App () {
-  console.log('hello') ;
+  console.log('hello')
 
   return (
-    <div>
-    <Navbar/>
-    </div>
-  );
+    <>
+      <div className='page_container'>
+        <Router className='wrep_container'>
+          <Navbar />
+
+        </Router>
+      </div>
+      <Footer />
+    </>
+  )
 }
 
-export default App;
+export default App
