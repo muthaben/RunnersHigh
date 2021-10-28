ead230a487564d17f0b94248569640b6bff54c51
kimbro97 <rlagudwog@gmail.com>
2021-10-28 14:57:18 +0900

    Edit userinfo,signout

--- server/controllers/user/signout.js
+++ server/controllers/user/signout.js
@@ -1,4 +1,18 @@
 // 회원탈퇴
-module.exports = (req, res) => {
-   
+const { isAuthorized } = require('../../functions')
+const { user } = require('../../models')
+
+module.exports = async (req, res) => {
+  const accessTokenData = isAuthorized(req)
+
+  if (!accessTokenData) {
+    res.status(401).send({ message: '회원탈퇴 성공했습니다.' })
+  } else {
+    await user.destory({
+      where: {
+        id: accessTokenData.id
+      }
+    })
+    res.status(200).send({ message: '회원탈퇴 성공했습니다.' })
+  }
 }
--- server/controllers/user/userinfo.js
+++ server/controllers/user/userinfo.js
@@ -1,18 +1,4 @@
 // 회원정보 조회
-const { isAuthorized } = require('../../functions')
-const { user } = require('../../models')
-
-module.exports = async (req, res) => {
-  const accessTokenData = isAuthorized(req)
-
-  if (!accessTokenData) {
-    res.status(401).send({ message: '회원탈퇴 성공했습니다.' })
-  } else {
-    await user.destory({
-      where: {
-        id: accessTokenData.id
-      }
-    })
-    res.status(200).send({ message: '회원탈퇴 성공했습니다.' })
-  }
+module.exports = (req, res) => {
+   
 }
