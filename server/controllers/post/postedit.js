17a9a32a887fe879b02644579dbb5f8cdd1c59ac
kimbro97 <rlagudwog@gmail.com>
2021-11-01 16:04:22 +0900

    Edit postedit, userinfoedit

--- server/controllers/post/postedit.js
+++ server/controllers/post/postedit.js
@@ -1,4 +1,58 @@
 // 게시물 수정
-module.exports = (req, res) => {
-    
+const { post } = require('../../models')
+const { isAuthorized } = require('../../functions/token')
+
+module.exports = async (req, res) => {
+  const accessTokenData = isAuthorized(req)
+
+  if (!accessTokenData) { // 토큰이 유효하지 않다면
+    return res.status(401).send({ message: '유효하지 않은 토큰입니다' })
+  }
+  if (req.file || Object.keys(req.body).length > 0) {
+    if (req.file) {
+      await post.update({
+        thumbnail_url: req.file.location
+      },
+      { where: { id: req.params.postid } }
+      )
+    }
+    if (req.body.title) {
+      await post.update({
+        title: req.body.title
+      },
+      { where: { id: req.params.postid } }
+      )
+    }
+    if (req.body.text) {
+      await post.update({
+        text: req.body.text
+      },
+      { where: { id: req.params.postid } }
+      )
+    }
+    if (req.body.location) {
+      await post.update({
+        location: req.body.location
+      },
+      { where: { id: req.params.postid } }
+      )
+    }
+    if (req.body.latitude) {
+      await post.update({
+        latitude: req.body.latitude
+      },
+      { where: { id: req.params.postid } }
+      )
+    }
+    if (req.body.longitude) {
+      await post.update({
+        longitude: req.body.longitude
+      },
+      { where: { id: req.params.postid } }
+      )
+    }
+    res.status(201).send({ message: '닉네임, 비밀번호 또는 프로필 이미지 변경에 성공했습니다' })
+  } else {
+    res.status(422).send({ message: '변경할 정보가 필요합니다' })
+  }
 }
--- server/controllers/user/userinfoedit.js
+++ server/controllers/user/userinfoedit.js
@@ -4,35 +4,44 @@ const { isAuthorized } = require('../../functions/token')
 
 module.exports = async (req, res) => {
   const accessTokenData = isAuthorized(req)
-  console.log(req.file, req.body)
-  if (!accessTokenData) {
-    res.status(401).send({ message: '유효하지 않은 토큰입니다' })
-  } else {
-    if (req.file || !req.body.nickname || !req.body.password) {
-      res.status(422).send({ message: '변경할 정보가 필요합니다' })
-    } else {
-      if (req.file) {
-        await user.update({
-          image_url: req.file.location
-        },
-        { where: { id: accessTokenData.id } }
-        )
-      }
-      if (req.body.nickname) {
-        await user.update({
-          nickname: req.body.nickname
-        },
-        { where: { id: accessTokenData.id } }
-        )
-      }
-      if (req.body.password) {
-        await user.update({
-          password: req.body.password
-        },
-        { where: { id: accessTokenData.id } }
-        )
-      }
-      res.status(201).send({ message: '닉네임, 비밀번호 또는 프로필 이미지 변경에 성공했습니다' })
+
+  if (!accessTokenData) { // 토큰이 유효하지 않다면
+    return res.status(401).send({ message: '유효하지 않은 토큰입니다' })
+  }
+
+  if (req.file || Object.keys(req.body).length > 0) {
+    if (req.file) {
+      await user.update({
+        image_url: req.file.location
+      },
+      { where: { id: accessTokenData.id } }
+      )
     }
+    if (req.body.nickname) {
+      await user.update({
+        nickname: req.body.nickname
+      },
+      { where: { id: accessTokenData.id } }
+      )
+    }
+    if (req.body.password) {
+      await user.update({
+        password: req.body.password
+      },
+      { where: { id: accessTokenData.id } }
+      )
+    }
+    res.status(201).send({ message: '닉네임, 비밀번호 또는 프로필 이미지 변경에 성공했습니다' })
+  } else {
+    res.status(422).send({ message: '변경할 정보가 필요합니다' })
   }
 }
+
+// 일단 토큰이 유효한지 확인한다
+// 1. 유효하지않으면 401오 거른다
+// 2. 유효하다면
+// 2.1
+
+// res.status(201).send({ message: '닉네임, 비밀번호 또는 프로필 이미지 변경에 성공했습니다' })
+// res.status(401).send({ message: '유효하지 않은 토큰입니다' })
+// res.status(422).send({ message: '변경할 정보가 필요합니다' })
