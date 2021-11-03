df8cc004ae44a3b540eb264f903183fa65432902
LeeGyuHwang <el@Appleui-MacBookAir.local>
2021-11-03 14:50:27 +0900

    add  mypage, mainpage

--- client/src/pages/MyPage.js
+++ client/src/pages/MyPage.js
@@ -1,37 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
+import PostListCard from '../components/PostListCard'
 import '../stylesheet/mypage.css'
+import Avatar from '@mui/material/Avatar'
+import Stack from '@mui/material/Stack'
 
 function MyPage () {
+  const [editProfile , setEditProfile] = useState(false)
+  const editHandle= () => {
+    setEditProfile(!editProfile)
+  }
   return (
-    <div className='mypage_container'>
-      <div className='mypage_profile'>
-        <div>profile </div>
-        <div>
-          nickname
-        </div>
-        <div>
-          email
-        </div>
-
-        <button>프로필수정</button>
-      </div>
-      <div className='mypage_cards'>
-        <div className='mypage_image'>이미지</div>
-        <div className='mypage_text'>본문</div>
-        <div className='mypage_footer'>
-          <span>작성자</span>
-          <span>작성일</span>
-        </div>
-      </div>
-      <div className='mypage_cards'>
-        <div className='mypage_image'>이미지</div>
-        <div className='mypage_text'>본문</div>
-        <div className='mypage_footer'>
-          <span>작성자</span>
-          <span>작성일</span>
-        </div>
-      </div>
-    </div>
+     <div className='mypage_container'>
+       {editProfile ?
+          <div className='mypage_profile'>
+          <div className='mypage_profile1'>
+          <div className='mypage_myimg'>
+          <Avatar
+         alt="Remy Sharp"
+         src="/static/images/avatar/1.jpg"
+         sx={{ width: 116, height: 116 }}
+       />
+          </div>
+          </div>
+          <div className='mypage_profile2'>
+          <input className='mypage_nickname' placeholder='Nickname' />
+          <input className='mypage_email' placeholder='Email' />
+ 
+          <span className='mypage_confirmEdit'>
+            <button onClick={editHandle}>수정완료</button> 
+            <button>회원탈퇴</button>
+         </span>
+         
+          </div>
+          </div>
+     : 
+       <div className='mypage_profile'>
+         <div className='mypage_profile1'>
+         <div className='mypage_myimg'>
+         <Avatar
+        alt="Remy Sharp"
+        src="/static/images/avatar/1.jpg"
+        sx={{ width: 116, height: 116 }}
+      />
+         </div>
+         </div>
+         <div className='mypage_profile2'>
+         <div className='mypage_nickname'>nickname</div>
+         <div className='mypage_email'>Email</div>
+         <button className='mypage_editbutton' onClick={editHandle}>프로필수정</button>
+         </div>
+         </div>
+        }
+       
+       {/* <div className='mypage_mypost'>
+         <PostListCard />
+         <PostListCard />
+         <PostListCard />
+         <PostListCard />
+         <PostListCard />
+         <PostListCard />
+       </div> */}
+       <div className='mypage_noting'>
+         게시글이 없습니다.
+       </div>
+     </div>
   )
 }
 
