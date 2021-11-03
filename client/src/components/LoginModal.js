9259747f958541e32342c0fe70e59145a135e40d
Sungrinhan <sungrin92@gmail.com>
2021-11-03 09:03:19 +0900

    Edit Login, Loginmodal, Signup

--- client/src/components/LoginModal.js
+++ client/src/components/LoginModal.js
@@ -2,18 +2,24 @@ import Login from './Login'
 import Signup from './Signup'
 import '../stylesheet/loginmodal.css'
 import { useState } from 'react'
+import CloseIcon from '@mui/icons-material/Close'
 function LoginModal ({ showModal, OpenModal }) {
   const [selectLogin, setSelectLogin] = useState(false)
 
   const ChangeSelect = () => {
     setSelectLogin(!selectLogin)
   }
+  const doubleFunction = () => {
+    OpenModal()
+    setSelectLogin(false)
+  }
   return (
     <div>
       {showModal
         ? <div className='loginmodal_background'>
-          <div onClick={OpenModal}>x</div>
+
           <div className='loginmodal_container'>
+            <CloseIcon className='loginmodal_icon' onClick={doubleFunction} />
             {selectLogin
               ? <Signup ChangeSelect={ChangeSelect} />
               : <Login ChangeSelect={ChangeSelect} />}
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,5 +1,7 @@
 import '../stylesheet/signup.css'
-
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
+import Grid from '@mui/material/Grid'
 function Signup ({ ChangeSelect }) {
   return (
     <div className='signup_container'>
@@ -7,12 +9,52 @@ function Signup ({ ChangeSelect }) {
         Signup
       </h3>
       <form className='signup_form'>
-        <input placeholder='Email' />
-        <input placeholder='Nickname' />
-        <input placeholder='Password' />
-        <input placeholder='Confirm Password' />
+        <TextField
+          margin='normal'
+          required
+          fullWidth
+          id='email'
+          label='Email Address'
+          name='email'
+        />
+        <TextField
+          margin='normal'
+          required
+          fullWidth
+          id='nickname'
+          label='Nickname'
+          name='nickname'
+
+        />
+        <TextField
+          margin='normal'
+          required
+          fullWidth
+          name='password'
+          label='Password'
+          type='password'
+          id='password'
+
+        />
+        <TextField
+          margin='normal'
+          required
+          fullWidth
+          name='confirm password'
+          label='Confirm Password'
+          type='confirm password'
+          id='confirm password'
+
+        />
       </form>
-      <button className='signup_btn'>Signup</button>
+      <Button
+        type='submit'
+        fullWidth
+        variant='contained'
+        sx={{ mt: 3, mb: 2 }}
+      >
+        Sign Up
+      </Button>
       <div onClick={ChangeSelect} className='signup_tosignup'>아이디가 있으십니까?</div>
     </div>
   )
