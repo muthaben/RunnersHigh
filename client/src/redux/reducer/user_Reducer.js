e90b150fa8e7e8ddd86978a1ac33c476c4ac07f1
Sungrinhan <sungrin92@gmail.com>
2021-11-05 14:05:22 +0900

    Edit login,signup, loginmodal, user_Reducer

--- client/src/redux/reducer/user_Reducer.js
+++ client/src/redux/reducer/user_Reducer.js
@@ -1,16 +1,12 @@
-import { SET_USERINFO } from '../action/index'
+import { SET_USERINFO, SET_ISLOGIN } from '../action/index'
+import { initialState } from './Initialstate'
 
-const initialState = {
-  userinfo: {
-    email: '',
-    password: ''
-  }
-}
 const itemReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_USERINFO:
       return Object.assign({}, state, { userinfo: action.payload })
-
+    case SET_ISLOGIN:
+      return Object.assign({}, state, { isLogin: action.payload })
     default :
       return state
   }
