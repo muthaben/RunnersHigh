09f69dee03f322bc18ab901634e212011ae0d0e9
Sungrinhan <sungrin92@gmail.com>
2021-11-14 17:05:08 +0900

    Edit chat bug

--- server/controllers/chat/messages.js
+++ server/controllers/chat/messages.js
@@ -5,7 +5,7 @@ module.exports = async (req, res) => {
   try {
     const chatInfo = await chatting.findAll({
       include: [
-        { model: user, attributes: ['nickname', 'image_url'] }
+        { model: user, attributes: ['nickname', 'image_url', 'id'] }
       ]
     })
     res.status(200).send({ data: chatInfo, message: '룸에대한 채팅 목록을 가져왔습니다' })
